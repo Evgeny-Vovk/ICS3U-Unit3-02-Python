e2ae70091a4c0304b14f92aa28ed9c249b1990d8
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-10-12 22:53:33 +0000

    fix

---
+++ ICS3U-Unit3-02.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: October 2022
+# ICS3U-Unit3-02.py File, learning if statements in python.
+
+import constants
+
+
+def main():
+
+    # input
+    guess_number = int(input("Guess the number from 0 to 9: "))
+
+    # process and output
+    if guess_number == constants.NUMBER:
+        print("\nYou guessed right.")
+    if guess_number != constants.NUMBER:
+        print("\nYou guessed wrong, try again.")
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: October 2022
+# constants file for ICS3U-Unit3-02.py.
+
+NUMBER = 5
